507d3302d61409cfe0ad4f78699ed9359cd4e49f
PavelAnkl <kluev-pa@yandex.ru>
2024-05-29 06:53:35 +0300

    end

--- main.py
+++ main.py
@@ -1,39 +1,16 @@
-import time
-import csv
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+import scrapy
 
-driver = webdriver.Chrome()
 
-url = "https://penza.hh.ru/vacancies/programmist"
+class DivannewparsSpider(scrapy.Spider):
+    name = "divannewpars"
+    allowed_domains = ["https://divan.ru"]
+    start_urls = ["https://www.divan.ru/penza/category/svet"]
 
-driver.get(url)
-
-time.sleep(3)
-
-vacancies = driver.find_elements(By.CLASS_NAME, 'vacancy-card--H8LvOiOGPll0jZvYpxIF')
-
-# Выводим вакансии на экран
-# print(vacancies)
-parsed_data = []
-
-for vacancy in vacancies:
-    try:
-        title = vacancy.find_element(By.CSS_SELECTOR, 'span.vacancy-name--SYbxrgpHgHedVTkgI_cA').text
-        company = vacancy.find_element(By.CSS_SELECTOR, 'span.company-info-text--O32pGCRW0YDmp3BHuNOP').text
-        salary = vacancy.find_element(By.CSS_SELECTOR, 'span.compensation-text--cCPBXayRjn5GuLFWhGTJ').text
-        link = vacancy.find_element(By.CSS_SELECTOR, 'a.bloko-link').get_attribute('href')
-    except:
-        print("произошла ошибка при парсинге")
-        continue
-
-    parsed_data.append([title, company, salary, link])
-
-driver.quit()
-
-with open("hh.csv", 'w',newline='', encoding='utf-8') as file:
-    writer = csv.writer(file)
-    writer.writerow(['Название вакансии', 'название компании', 'зарплата', 'ссылка на вакансию'])
-    writer.writerows(parsed_data)
-#
-# Чтобы удобнее просмотреть результат, открываем файл через программу для чтения и редактирования таблиц.
+    def parse(self, response):
+        divans = response.css('div._Ud0k')
+        for divan in divans:
+            yield {
+                'name': divan.css('div.lsooF span::text').get(),
+                'price': divan.css('div.pY3d2 span::text').get(),
+                'url': divan.css('a').attrib['href']
+            }
